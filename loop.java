bcc578757dd40b42813a339fb26395fb95498a4e
GayathriBalakumaran <77260500+GayathriBalakumaran@users.noreply.github.com>
2021-01-11 22:20:09 +0530

    Add files via upload

---
+++ loop.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class loop{
+    public static void main(String [] args){
+        Scanner kbd=new Scanner(System.in);
+        int N;
+        N=kbd.nextInt();
+        for(int i=1;i<=N;i++){
+            System.out.println(i+" "+"Abracadabra");  
+        }
+    }
+}
